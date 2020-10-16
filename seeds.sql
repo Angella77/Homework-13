54f189d9bcc4d121d3d317b0599cfdd2cb2f7f93
Angella <angiejeanlewis@gmail.com>
2020-10-15 23:18:52 -0400

    update seeds sql

--- seeds.sql
+++ seeds.sql
@@ -1,2 +1,6 @@
-INSERT INTO burgers (burger_name)
-VALUES ("Culver's Butter Burger")
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('chicken');
+INSERT INTO burgers (burger_name) VALUES ('beed');
+INSERT INTO burgers (burger_name, devoured) VALUES ('veggie', true);
+INSERT INTO burgers (burger_name) VALUES ('cow');
